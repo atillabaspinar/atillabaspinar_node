29953b235d7c75aa674b4d64d502b73bf6077c82
atilla baspinar <abaspinar@isravision.com>
2019-08-22 15:02:00 +0200

    mongo

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,11 @@ const articleController = require('../controllers/article');
 
 const router = express.Router();
 
+
 router.get('/articles', articleController.getArticles);
 
 router.post('/articles', articleController.postArticles);
+
+router.post('/article', articleController.postAddArticle);
 
 module.exports = router;
